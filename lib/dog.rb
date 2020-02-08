5ccb11f904ebee10e88cfd13a0b9af5015b2f002
Samantha Meason <sbmeason@gmail.com>
2020-02-08 21:27:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+end
+
+def name=(name, breed)
+  @name = name
+  @breed = breed
+end
+
+def name_breed
+  @name
+  @breed
+end
+
+fido = Dog.new("Fido", "Pug")
+
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
+fido = Dog.new("Fido")
